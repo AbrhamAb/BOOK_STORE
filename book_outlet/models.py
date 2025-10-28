1d125833d551b8ae3532bf5695288f40651d813a
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-10-28 10:39:39 +0300

    .

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.utils.text import slugify   
 # Create your models here.
 
 class book(models.Model):
@@ -12,6 +12,11 @@ class book(models.Model):
     isbn_number = models.CharField(max_length=13, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.PositiveIntegerField()
+    slug = models.SlugField(max_length=200,default="")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -5,6 +5,6 @@ from .models import book
 def index(request):
     books = book.objects.all()
     return render(request, 'book_outlet/index.html', {'books': books})
-def book_detail(request, book_id):
-    book_instance = book.objects.get(id=book_id)
+def book_detail(request, slug):
+    book_instance = book.objects.get(slug=slug)
     return render(request, 'book_outlet/book_detail.html', {'book': book_instance})
