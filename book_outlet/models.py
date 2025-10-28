475c92c17c922bbc9f94a494f7904726891f0eb0
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-10-28 15:48:34 +0300

    work with models and relationships between models

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,22 +1,27 @@
+from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.utils.text import slugify   
+from django.urls import reverse
+from django.utils.text import slugify
+
 # Create your models here.
 
-class book(models.Model):
-    title = models.CharField(max_length=200)
-    rating = models.FloatField(default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
-    author = models.CharField(max_length=100)
-    is_best_seller = models.BooleanField(default=False)
-    published_date = models.DateField()
-    isbn_number = models.CharField(max_length=13, unique=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    slug = models.SlugField(max_length=200,default="")
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    is_bestselling = models.BooleanField(default=False)
+    # Harry Potter 1 => harry-potter-1
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.rating})"
